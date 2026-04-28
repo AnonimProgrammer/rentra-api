945314e4b26d0c81a347731a10c1ef61e75ed0c5
AnonimProgrammer <zephyr11ff@gmail.com>
2026-04-28 21:27:18 +0400

    refactor: Clean-ups

--- src/main/java/com/rentra/controller/VehicleController.java
+++ src/main/java/com/rentra/controller/VehicleController.java
@@ -3,8 +3,6 @@ package com.rentra.controller;
 import java.util.List;
 import java.util.UUID;
 
-import com.rentra.service.security.auth.AuthService;
-import com.rentra.service.user.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +10,7 @@ import com.rentra.dto.vehicle.CreateVehicleRequest;
 import com.rentra.dto.vehicle.VehicleDetails;
 import com.rentra.dto.vehicle.VehicleSearchRequest;
 import com.rentra.dto.vehicle.VehicleSummary;
+import com.rentra.service.security.auth.AuthService;
 import com.rentra.service.vehicle.VehicleService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -22,14 +21,15 @@ import lombok.RequiredArgsConstructor;
 public class VehicleController {
     private final VehicleService vehicleService;
     private final AuthService authService;
+
     @PostMapping
     public VehicleDetails createVehicle(@Valid @RequestBody CreateVehicleRequest request) {
         return vehicleService.create(request);
     }
 
     @PostMapping("/{id}/technical-check/complete")
-    public ResponseEntity<VehicleSummary> completeTechnicalCheck(@PathVariable("id") UUID id){
-        return ResponseEntity.ok(vehicleService.completeTechnicalCheck(id,authService.getCurrentUserId()));
+    public ResponseEntity<VehicleSummary> completeTechnicalCheck(@PathVariable("id") UUID id) {
+        return ResponseEntity.ok(vehicleService.completeTechnicalCheck(id, authService.getCurrentUserId()));
     }
 
     @GetMapping("/{id}")
